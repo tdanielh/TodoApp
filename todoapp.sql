0bf15564028d13784c16e5e987b823dc98c157d9
git <tommy.d.hansen@gmail.com>
2018-12-01 11:24:38 +0100

    SQL create og SQL data

---
+++ todoapp.sql
@@ -0,0 +1,40 @@
+CREATE TABLE `lists` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY(`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE tasks (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` enum('todo','done') NOT NULL DEFAULT 'todo',
+  `list_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY(`id`, `list_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY(`id`)
+) ENGINE=InnoDB;
+
+ALTER TABLE `tasks`
+	ADD FOREIGN KEY (list_id) REFERENCES lists(id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE;
+	
+ALTER TABLE `lists`
+	ADD FOREIGN KEY (user_id) REFERENCES users(id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE;
+	
+	
+INSERT INTO `users` (`id`, `name`, `password`, `email`) VALUES
+(1, 'Kasper', '$2y$10$eZ3SDxLnmIqSP4YF5cAKi.woj9UmjgxdZ/sU1x1F5v2ei9KUgwpmS', 'kasper@test.com'),
+(2, 'Jesper', '$2y$10$eZ3SDxLnmIqSP4YF5cAKi.woj9UmjgxdZ/sU1x1F5v2ei9KUgwpmS', 'jesper@test.com'),
+(3, 'Jonatan', '$2y$10$eZ3SDxLnmIqSP4YF5cAKi.woj9UmjgxdZ/sU1x1F5v2ei9KUgwpmS', 'jonatan@test.com');
